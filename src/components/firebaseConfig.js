35f07aa703845651b6521458e1f6666d21a383f9
Nikki Petersen <piknik@me.com>
2020-08-08 05:30:35 -0600

    basic firebase setup

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyA7b2Or977mso6NYicIF_4dBY_yQDFZfaI",
+  authDomain: "vuefs-prod-72776.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-72776.firebaseio.com",
+  projectId: "vuefs-prod-72776",
+  storageBucket: "vuefs-prod-72776.appspot.com",
+  messagingSenderId: "780783461417",
+  appId: "1:780783461417:web:37002f76cfafd5919fe838"
+}
